b35a80def921139078f519324e46d9e214828fe7
con2222 <99406331+con2222@users.noreply.github.com>
2022-02-27 13:37:15 +0700

    Update index.js

--- index.js
+++ index.js
@@ -15,56 +15,56 @@ const app = new express();
 const server = http.createServer(app);
 const io = new Server(server);
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-// Set public directory
+
 app.use(express.static(path.join(__dirname, "public")));
 
-// const "/" = "chat";
 
-// this block will run when the client connects
+
+
 io.on("connection", (socket) => {
   socket.on("joinRoom", ({ username }) => {
     const user = setNewUser(socket.id, username);
 
     socket.join("/");
 
-    // General welcome
+
     socket.emit(
       "message",
-      formatMessage("WebChat", "You have joined the chat ")
+      formatMessage("WebChat", "Вы присоединились к комнате")
     );
 
-    // Broadcast everytime users connects
+
     socket.broadcast
       .to("/")
       .emit(
         "message",
-        formatMessage("WebChat", `${user.username} has joined the room`)
+        formatMessage("WebChat", `${user.username} вошел в комнату`)
       );
 
-    // Current active users
+
     io.to("/").emit("roomUsers", {
       users: getUsers("/"),
     });
   });
 
-  // Listen for client message
+
   socket.on("chatMessage", (msg) => {
     const user = getActiveUser(socket.id);
 
     io.to("/").emit("message", formatMessage(user.username, msg));
   });
 
-  // Runs when client disconnects
+
   socket.on("disconnect", () => {
     const user = exitRoom(socket.id);
 
     if (user) {
       io.to("/").emit(
         "message",
-        formatMessage("WebChat", `${user.username} has left the room`)
+        formatMessage("WebChat", `${user.username} покинул комнату`)
       );
 
-      // Current active users
+
       io.to("/").emit("roomUsers", {
         users: getUsers("/"),
       });
